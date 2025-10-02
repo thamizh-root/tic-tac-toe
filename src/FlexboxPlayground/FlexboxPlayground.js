0d1a8b63ac12b0c05647a8aa7b3e400494c20939
Thamizh <thamizh@happy.local>
2025-10-02 18:49:02 +0530

    added playground for flexbox

---
+++ src/FlexboxPlayground/FlexboxPlayground.js
@@ -0,0 +1,221 @@
+import { useState } from "react";
+import "./FlexboxPlayground.css";
+
+export default function FlexboxPlayground() {
+  return (
+    <>
+
+      <div className="full">
+
+
+      <div class="box">
+        <div class="one">One</div>
+        <div class="two">In Petersburg at that time a complicated struggle was
+being carried on with greater heat than ever in the highest
+circles, between the parties of Rumyantsev, the French,
+Marya Fedorovna, the Tsarevich, and others, drowned as
+usual by the buzzing of the court drones. But the calm,
+luxurious life of Petersburg, concerned only about
+phantoms and reflections of real life, went on in its old
+way and made it hard, except by a great effort, to realize
+the danger and the difficult position of the Russian
+people. There were the same receptions and balls, the
+same French theater, the same court interests and service
+interests and intrigues as usual. Only in the very highest
+circles were attempts made to keep in mind the
+difficulties of the actual position. Stories were whispered
+of how differently the two Empresses behaved in these
+difficult circumstances. The Empress Marya, concerned
+for the welfare of the charitable and educational
+institutions under her patronage, had given directions that
+they should all be removed to Kazan, and the things
+belonging to these institutions had already been packed
+up. The Empress Elisabeth, however, when asked what
+instructions she would be pleased to give- with her
+2199 of 2882
+War and Peace
+characteristic Russian patriotism had replied that she
+could give no directions about state institutions for that
+was the affair of the sovereign, but as far as she
+personally was concerned she would be the last to quit
+Petersburg.
+At Anna Pavlovna’s on the twenty-sixth of August, the
+very day of the battle of Borodino, there was a soiree, the
+chief feature of which was to be the reading of a letter
+from His Lordship the Bishop when sending the Emperor
+an icon of the Venerable Sergius. It was regarded as a
+model of ecclesiastical, patriotic eloquence. Prince Vasili
+himself, famed for his elocution, was to read it. (He used
+to read at the Empress’.) The art of his reading was
+supposed to lie in rolling out the words, quite
+independently of their meaning, in a loud and singsong
+voice alternating between a despairing wail and a tender
+murmur, so that the wail fell quite at random on one word
+and the murmur on another. This reading, as was always
+the case at Anna Pavlovna’s soirees, had a political
+significance. That evening she expected several important
+personages who had to be made ashamed of their visits to
+the French theater and aroused to a patriotic temper. A
+good many people had already arrived, but Anna
+Pavlovna, not yet seeing all those whom she wanted in.
+
+In Petersburg at that time a complicated struggle was
+being carried on with greater heat than ever in the highest
+circles, between the parties of Rumyantsev, the French,
+Marya Fedorovna, the Tsarevich, and others, drowned as
+usual by the buzzing of the court drones. But the calm,
+luxurious life of Petersburg, concerned only about
+phantoms and reflections of real life, went on in its old
+way and made it hard, except by a great effort, to realize
+the danger and the difficult position of the Russian
+people. There were the same receptions and balls, the
+same French theater, the same court interests and service
+interests and intrigues as usual. Only in the very highest
+circles were attempts made to keep in mind the
+difficulties of the actual position. Stories were whispered
+of how differently the two Empresses behaved in these
+difficult circumstances. The Empress Marya, concerned
+for the welfare of the charitable and educational
+institutions under her patronage, had given directions that
+they should all be removed to Kazan, and the things
+belonging to these institutions had already been packed
+up. The Empress Elisabeth, however, when asked what
+instructions she would be pleased to give- with her
+2199 of 2882
+War and Peace
+characteristic Russian patriotism had replied that she
+could give no directions about state institutions for that
+was the affair of the sovereign, but as far as she
+personally was concerned she would be the last to quit
+Petersburg.
+At Anna Pavlovna’s on the twenty-sixth of August, the
+very day of the battle of Borodino, there was a soiree, the
+chief feature of which was to be the reading of a letter
+from His Lordship the Bishop when sending the Emperor
+an icon of the Venerable Sergius. It was regarded as a
+model of ecclesiastical, patriotic eloquence. Prince Vasili
+himself, famed for his elocution, was to read it. (He used
+to read at the Empress’.) The art of his reading was
+supposed to lie in rolling out the words, quite
+independently of their meaning, in a loud and singsong
+voice alternating between a despairing wail and a tender
+murmur, so that the wail fell quite at random on one word
+and the murmur on another. This reading, as was always
+the case at Anna Pavlovna’s soirees, had a political
+significance. That evening she expected several important
+personages who had to be made ashamed of their visits to
+the French theater and aroused to a patriotic temper. A
+good many people had already arrived, but Anna
+Pavlovna, not yet seeing all those whom she wanted in
+In Petersburg at that time a complicated struggle was
+being carried on with greater heat than ever in the highest
+circles, between the parties of Rumyantsev, the French,
+Marya Fedorovna, the Tsarevich, and others, drowned as
+usual by the buzzing of the court drones. But the calm,
+luxurious life of Petersburg, concerned only about
+phantoms and reflections of real life, went on in its old
+way and made it hard, except by a great effort, to realize
+the danger and the difficult position of the Russian
+people. There were the same receptions and balls, the
+same French theater, the same court interests and service
+interests and intrigues as usual. Only in the very highest
+circles were attempts made to keep in mind the
+difficulties of the actual position. Stories were whispered
+of how differently the two Empresses behaved in these
+difficult circumstances. The Empress Marya, concerned
+for the welfare of the charitable and educational
+institutions under her patronage, had given directions that
+they should all be removed to Kazan, and the things
+belonging to these institutions had already been packed
+up. The Empress Elisabeth, however, when asked what
+instructions she would be pleased to give- with her
+2199 of 2882
+War and Peace
+characteristic Russian patriotism had replied that she
+could give no directions about state institutions for that
+was the affair of the sovereign, but as far as she
+personally was concerned she would be the last to quit
+Petersburg.
+At Anna Pavlovna’s on the twenty-sixth of August, the
+very day of the battle of Borodino, there was a soiree, the
+chief feature of which was to be the reading of a letter
+from His Lordship the Bishop when sending the Emperor
+an icon of the Venerable Sergius. It was regarded as a
+model of ecclesiastical, patriotic eloquence. Prince Vasili
+himself, famed for his elocution, was to read it. (He used
+to read at the Empress’.) The art of his reading was
+supposed to lie in rolling out the words, quite
+independently of their meaning, in a loud and singsong
+voice alternating between a despairing wail and a tender
+murmur, so that the wail fell quite at random on one word
+and the murmur on another. This reading, as was always
+the case at Anna Pavlovna’s soirees, had a political
+significance. That evening she expected several important
+personages who had to be made ashamed of their visits to
+the French theater and aroused to a patriotic temper. A
+good many people had already arrived, but Anna
+Pavlovna, not yet seeing all those whom she wanted in</div>
+        <div class="three">In Petersburg at that time a complicated struggle was
+being carried on with greater heat than ever in the highest
+circles, between the parties of Rumyantsev, the French,
+Marya Fedorovna, the Tsarevich, and others, drowned as
+usual by the buzzing of the court drones. But the calm,
+luxurious life of Petersburg, concerned only about
+phantoms and reflections of real life, went on in its old
+way and made it hard, except by a great effort, to realize
+the danger and the difficult position of the Russian
+people. There were the same receptions and balls, the
+same French theater, the same court interests and service
+interests and intrigues as usual. Only in the very highest
+circles were attempts made to keep in mind the
+difficulties of the actual position. Stories were whispered
+of how differently the two Empresses behaved in these
+difficult circumstances. The Empress Marya, concerned
+for the welfare of the charitable and educational
+institutions under her patronage, had given directions that
+they should all be removed to Kazan, and the things
+belonging to these institutions had already been packed
+up. The Empress Elisabeth, however, when asked what
+instructions she would be pleased to give- with her
+2199 of 2882
+War and Peace
+characteristic Russian patriotism had replied that she
+could give no directions about state institutions for that
+was the affair of the sovereign, but as far as she
+personally was concerned she would be the last to quit
+Petersburg.
+At Anna Pavlovna’s on the twenty-sixth of August, the
+very day of the battle of Borodino, there was a soiree, the
+chief feature of which was to be the reading of a letter
+from His Lordship the Bishop when sending the Emperor
+an icon of the Venerable Sergius. It was regarded as a
+model of ecclesiastical, patriotic eloquence. Prince Vasili
+himself, famed for his elocution, was to read it. (He used
+to read at the Empress’.) The art of his reading was
+supposed to lie in rolling out the words, quite
+independently of their meaning, in a loud and singsong
+voice alternating between a despairing wail and a tender
+murmur, so that the wail fell quite at random on one word
+and the murmur on another. This reading, as was always
+the case at Anna Pavlovna’s soirees, had a political
+significance. That evening she expected several important
+personages who had to be made ashamed of their visits to
+the French theater and aroused to a patriotic temper. A
+good many people had already arrived, but Anna
+Pavlovna, not yet seeing all those whom she wanted in</div>
+      </div>
+
+      <div class="container">
+        <div className="container-1">
+        <p>One</p>
+        </div>
+        <p className="container-2">Two</p>
+        <p className="container-3">Three</p>
+      </div>
+
+      </div>
+
+
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -8,13 +8,15 @@ import Game from './Game/Game';
 import SandboxPlayground from './SandboxPlayground';
 import Trail from './Trail';
 import PercentageIsBetter from './PercentageIsBetter';
+import FlexboxPlayground from './FlexboxPlayground/FlexboxPlayground';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <FlexboxPlayground />
     {/* <WidthAlgorithm /> */}
-    <PercentageIsBetter />
+    {/* <PercentageIsBetter /> */}
     {/* <MarginCollapse /> */}
     {/* <HeightAlgorithm /> */}
     {/* <SandboxPlayground /> */}
